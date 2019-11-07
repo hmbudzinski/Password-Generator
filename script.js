3a4d32fbff8d52e34fd01777966594c25e9a7d7a
Hayley Budzinski <hayleybudzinski@gmail.com>
2019-11-07 13:23:56 -0700

    added initial button confirms

--- script.js
+++ script.js
@@ -1,44 +1,91 @@
-//generate password 
-function generate(){
-    //set password length/complexity
-    let complexity = document.getElementById("slider").value;
+//set variables 
+//generate button 
+var genButton = document.querySelector("#genButton");
+// copy button 
+var copyButton = document.querySelector("#copyButton");
 
-    let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-=+";
+//arrays for password
+var lowerValues = "abcdefghijklmnopqrstuvwxyz".split;
+var upperValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split;
+var numValues = "1234567890".split;
+var symbolValues = "!@#$%^&*()_-=+".split;
 
-    let password = "";
-
-    //create for look to choose password characters
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-
-    //add password to textbox.display area
-    document.getElementById("display").value = password;
-
-    //add password to previously generated passwords section
-    document.getElementById("lastPasswords").innerHTML += password + "<br />";
-
-}
-
-//set default length display of 25
-document.getElementById("length"). innerHTML = "length: 25";
-
-//function to set length based on slider position
-document.getElementById("slider").oninput = function(){
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+//when user clicks generate password, ask if would like to include lowercase values 
+genButton.addEventListener("click", function(event){
+var lowercase = confirm("Do you want your password to use lowercase letters?");
+    
+    if(event){
+    console.log("they want lowercase");
     }
     else{
-        documents.getElementById("length").innerHTML = "Length: 1";
+    console.log("they do not want lowercase");
     }
-}
 
-//function to copy to clipboard
+var uppercase = confirm("Do you want your password to use uppercase letters?");
+   
+    if (event){
+    console.log("they want to use uppercase");
+    }
+    else{
+        console.log("they do not want uppercase");
+    }
 
-function copyPassword(){
-    document.getElementById("display").select();
+var symbols = confirm("Do you want your password to use symbols?");
 
-    document.execCommand("Copy");
+    if (event){
+    console.log("they want to use symbols");
+    }
+    else{
+        console.log("they do not want symbols");
+    }
 
-    alert("Password Copied to Clipboard!");
-}
+var numbers = confirm("Do you want your password to use numbers?");
+
+    if (event){
+    console.log("they want to use numbers");
+    }
+    else{
+        console.log("they do not want numbers");
+    }
+})
+
+//generate password 
+function generate(){
+    set password length/complexity
+    let complexity = document.getElementById("slider").value;
+
+
+//     let password = "";
+
+//     //create for loop to choose password characters
+//     for(var i = 0; i <= complexity; i++){
+//         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+//     }
+
+//     //add password to textbox.display area
+//     document.getElementById("display").value = password;
+
+// }
+
+// //set default length display of 60
+// document.getElementById("length"). innerHTML = "length: 60";
+
+// //function to set length based on slider position
+// document.getElementById("slider").oninput = function(){
+//     if(document.getElementById("slider").value > 0){
+//         document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+//     }
+//     else{
+//         documents.getElementById("length").innerHTML = "Length: 1";
+//     }
+// }
+
+// //function to copy to clipboard
+
+// function copyPassword(){
+//     document.getElementById("display").select();
+
+//     document.execCommand("Copy");
+
+//     alert("Password Copied to Clipboard!");
+// }
