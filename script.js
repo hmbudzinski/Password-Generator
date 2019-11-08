e2eb37a2c1b22a55ce53a7ee03e7c21c8aee16c7
Hayley Budzinski <hayleybudzinski@gmail.com>
2019-11-08 13:02:46 -0700

    trying to link my arrays to the random password generator

--- script.js
+++ script.js
@@ -9,63 +9,70 @@ var lowerValues = ["abcdefghijklmnopqrstuvwxyz"].split;
 var upperValues = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].split;
 var numValues = ["1234567890"].split;
 var symbolValues = ["!@#$%^&*()_-=+"].split;
+var allArray = ["lowerValues", "upperValues", "numValues", "symbolValues"];
 
 //variable for display
 var displayEl = document.getElementById("#display");
 
-//when user clicks generate password, ask if would like to include lowercase values 
+//when user clicks generate password 
 genButton.addEventListener("click", function(event){
 
 //determines if user wants lowercase
 var lowercase = confirm("Do you want your password to use lowercase letters?");
     
-    if(lowercase === true){
-    console.log("they want lowercase");
-    var lowercase = "lowerValues";
-    }
-    else{
-    console.log("they do not want lowercase");
+    if(lowercase){
+    var lowercase = lowerValues;
+        console.log("they want lowercase");
+    } else{
+        console.log("they do not want lowercase");
     }
 
 //determines is user wants uppercase
 var uppercase = confirm("Do you want your password to use uppercase letters?");
    
-    if (uppercase === true){
-    console.log("they want to use uppercase");
-    var uppercase = "upperValues";
-    }
-    else{
+    if (uppercase){
+    var uppercase = upperValues;
+        console.log("they want to use uppercase");
+    } else{
         console.log("they do not want uppercase");
     }
 
 //determines if user wants symbols
 var symbols = confirm("Do you want your password to use symbols?");
 
-    if (symbols === true){
-    console.log("they want to use symbols");
-    var symbols = "symbolValues";
-    }
-    else{
+    if (symbols){
+    var symbols = symbolValues;
+        console.log("they want to use symbols");
+    } else{
         console.log("they do not want symbols");
     }
 
 //determines if user wants numbers 
 var numbers = confirm("Do you want your password to use numbers?");
 
-    if (numbers === true){
-    console.log("they want to use numbers");
+    if (numbers){
     var numbers = "numValues";
-    }
-    else{
+        console.log("they want to use numbers");
+    } else{
         console.log("they do not want numbers");
     }
 
 //lets user decide what length
 var passLength = prompt("How long do you want your password to be? Choose a number between 1 and 120!");
     
-    if (passLength){
+    if (passLength > 0 && passLength < 121){
         console.log("Amount: " + passLength);
+        var passLength = "";
     }
+    else{
+        alert("Must enter amount between 1 and 120");
+        prompt("How long do you want your password to be? Choose a number between 1 and 120!");
+    }
+//if they type a number in, generate random password
+for (var i = 0; i <= passLength; i++){
+    var passGen = allArray[Math.floor(Math.random() * allArray.length)];
+}
+console.log("Lets see: " + passGen);
 })
 
 // end of choices 
@@ -82,14 +89,9 @@ var passLength = prompt("How long do you want your password to be? Choose a numb
     //     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     // }
     
-// //function to copy to clipboard
-
-// copyButton.addEventListener("click", function(event){
-
-// function.copyPassword(){
-//     document.getElementById("display").select();
-//     document.execCommand("Copy");
-//     }
-//     alert("Password Copied to Clipboard!");
-
-// })
+//function to copy to clipboard
+    copyButton.addEventListener("click", function(event){
+        document.getElementById("display").select();
+        document.execCommand("Copy");
+    })
+        alert("Password Copied to Clipboard!");
