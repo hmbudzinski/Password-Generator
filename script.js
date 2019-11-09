cd8f276ccf38ba7b52d1f31597b3b231b2f1073a
Hayley Budzinski <hayleybudzinski@gmail.com>
2019-11-09 10:00:10 -0700

    added js to fix random selectors

--- script.js
+++ script.js
@@ -5,14 +5,28 @@ var genButton = document.querySelector("#genButton");
 var copyButton = document.querySelector("#copyButton");
 
 //arrays for password
-var lowerValues = ["abcdefghijklmnopqrstuvwxyz"].split;
-var upperValues = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].split;
-var numValues = ["1234567890"].split;
-var symbolValues = ["!@#$%^&*()_-=+"].split;
-var allArray = ["lowerValues", "upperValues", "numValues", "symbolValues"];
+var lowerValues = ["abcdefghijklmnopqrstuvwxyz"];
+lowerValues[0].split("");
+console.log(lowerValues[0].split(""));
+
+var upperValues = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+lowerValues[0].split("");
+console.log(upperValues[0].split(""));
+
+var numValues = ["1234567890"];
+numValues[0].split("");
+console.log(numValues[0].split(""));
+
+var symbolValues = ["!@#$%^&*()_-=+"];
+numValues[0].split("");
+console.log(symbolValues[0].split(""));
+
 
 //variable for display
 var displayEl = document.getElementById("#display");
+
+//empty array to push to 
+var allArray = [ ];
 
 //when user clicks generate password 
 genButton.addEventListener("click", function(event){
@@ -21,8 +35,8 @@ genButton.addEventListener("click", function(event){
 var lowercase = confirm("Do you want your password to use lowercase letters?");
     
     if(lowercase){
-    var lowercase = lowerValues;
-        console.log("they want lowercase");
+    allArray = allArray.concat(lowerValues[0].split(""));
+        console.log(allArray);
     } else{
         console.log("they do not want lowercase");
     }
@@ -31,67 +45,72 @@ var lowercase = confirm("Do you want your password to use lowercase letters?");
 var uppercase = confirm("Do you want your password to use uppercase letters?");
    
     if (uppercase){
-    var uppercase = upperValues;
-        console.log("they want to use uppercase");
+    allArray = allArray.concat(upperValues[0].split(""));
+        console.log(allArray);
     } else{
         console.log("they do not want uppercase");
     }
 
-//determines if user wants symbols
+// //determines if user wants symbols
 var symbols = confirm("Do you want your password to use symbols?");
 
     if (symbols){
-    var symbols = symbolValues;
-        console.log("they want to use symbols");
+    allArray = allArray.concat(symbolValues[0].split(""));
+        console.log(allArray);
     } else{
         console.log("they do not want symbols");
     }
 
-//determines if user wants numbers 
+// //determines if user wants numbers 
 var numbers = confirm("Do you want your password to use numbers?");
 
     if (numbers){
-    var numbers = "numValues";
+        allArray = allArray.concat(numValues[0].split(""));
         console.log("they want to use numbers");
     } else{
         console.log("they do not want numbers");
     }
 
-//lets user decide what length
+// //lets user decide what length
 var passLength = prompt("How long do you want your password to be? Choose a number between 1 and 120!");
     
     if (passLength > 0 && passLength < 121){
         console.log("Amount: " + passLength);
-        var passLength = "";
     }
     else{
         alert("Must enter amount between 1 and 120");
         prompt("How long do you want your password to be? Choose a number between 1 and 120!");
     }
 //if they type a number in, generate random password
+var finalPass = [];
+
 for (var i = 0; i <= passLength; i++){
     var passGen = allArray[Math.floor(Math.random() * allArray.length)];
+    finalPass.push(passGen); 
+    console.log("Lets see: " + passGen);
 }
-console.log("Lets see: " + passGen);
+console.log(finalPass);
+    // return finalPass;
+
+document.getElementById("display").value = finalPass;
+
 })
 
-// end of choices 
 
 //need to generate pasword 
 //need to log users answers and only use what was selected 
 
 //add password to textbox.display area
-// document.getElementById("display").value = password;
+
     
-// }
 //create for loop to choose password characters
     // for(var i = 0; i <= complexity; i++){
     //     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     // }
     
 //function to copy to clipboard
-    copyButton.addEventListener("click", function(event){
-        document.getElementById("display").select();
-        document.execCommand("Copy");
-    })
-        alert("Password Copied to Clipboard!");
+    // copyButton.addEventListener("click", function(event){
+    //     document.getElementById("display").select();
+    //     document.execCommand("Copy");
+    // })
+    //     alert("Password Copied to Clipboard!");
